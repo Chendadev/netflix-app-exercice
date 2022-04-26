e7cf9bc1d1ec629f18b5eb14fc2f5c2d4266afe6
Net Chenda <netchenda@macbook-pro-de-net.home>
2022-04-26 23:42:52 +0200

    final css and front scroll horizontal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
 import data from "./assets/data.json"; // c'est deja une variable data, pas besoin de stockeer
+import logo from "./assets/img/logonetflix.jpg";
 
 function App() {
-  return data.map((dataMovie, index) => {
-    return <h1>{dataMovie.category}</h1>;
-  });
+  return (
+    <div className="main">
+      <div className="logo-container">
+        <img alt="netflix" src={logo} />
+      </div>
+      {data.map((dataMovie, index) => {
+        // console.log("images ?:", dataMovie.images);
+        return (
+          <>
+            <div className="title-container">
+              <h1>{dataMovie.category}</h1>
+            </div>
+            <div className="containerMovie">
+              {dataMovie.images.map((pictureMovie, index) => {
+                // console.log(pictureMovie);
+                return <img index={index} src={pictureMovie} alt="movies" />;
+              })}
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
 }
 export default App;
