ebda73b9aa72cc6b8ea01f0abfa9b7e2c2f01a66
Net Chenda <netchenda@mbp-de-net.lan>
2022-04-26 16:57:59 +0200

    display all title only with loop map method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import data from "./assets/data.json"; // c'est deja une variable data, pas besoin de stockeer
 
 function App() {
-  return <p>Hello</p>;
+  return data.map((dataMovie, index) => {
+    return <h1>{dataMovie.category}</h1>;
+  });
 }
 export default App;
