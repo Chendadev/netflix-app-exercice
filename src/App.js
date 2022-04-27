e433cfba388c333441182b71198494a32b234290
Net Chenda <netchenda@macbook-pro-de-net.home>
2022-04-27 07:50:17 +0200

    hide horizontal scrollbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import data from "./assets/data.json"; // c'est deja une variable data, pas besoin de stockeer
 import logo from "./assets/img/logonetflix.jpg";
+import Section from "./components/Section";
 
+const array = ["test", "ala", "con"]
 function App() {
   return (
     <div className="main">
@@ -15,6 +17,7 @@ function App() {
             <div className="title-container">
               <h1>{dataMovie.category}</h1>
             </div>
+
             <div className="containerMovie">
               {dataMovie.images.map((pictureMovie, index) => {
                 // console.log(pictureMovie);
@@ -22,9 +25,17 @@ function App() {
               })}
             </div>
           </>
-        );
+        )
       })}
+      {/* <div>
+        <Section
+          {array.map((lemot, index) => {
+            console.log("apartirdici :", lemot);
+            return <h1 key={index}>{lemot}</h1>
+          })} />
+      </div> */}
     </div>
-  );
+  )
 }
+
 export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+
+
+export default function Section(props) {
+    return (
+        <div>
+            <h1>{lemot}</h1>
+        </div>
+    )
+}
